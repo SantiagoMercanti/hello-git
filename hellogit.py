d2906cadbec3337a2c78a93042734bee86f493d3
SantiagoMercanti <santi.mercanti15@gmail.com>
2024-01-30 01:08:00 -0300

    Login veersion2

--- hellogit.py
+++ hellogit.py
@@ -1,7 +1,3 @@
 print("Hello Git!")
 print("Messi es The Best")
-<<<<<<< HEAD
-print("Este es despues de haber creado el login, y ahora lo toco en Login")
-=======
 print("Este es despues de haber creado el login, y lo toco en main")
->>>>>>> main
